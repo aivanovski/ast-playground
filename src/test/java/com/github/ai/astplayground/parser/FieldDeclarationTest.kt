cc95e8c47bbbba6e91d500b30d21c7e15c8f716e
Aliaksei Ivanouski <alexei.ivanovski@gmail.com>
2026-03-31 21:34:21 +0200

    Refactor FieldDeclarationTest

--- src/test/java/com/github/ai/astplayground/parser/FieldDeclarationTest.kt
+++ src/test/java/com/github/ai/astplayground/parser/FieldDeclarationTest.kt
@@ -23,134 +23,36 @@ import org.junit.jupiter.api.Test
 class FieldDeclarationTest {
 
     @Test
-    fun `should support boolean declarations`() {
+    fun `should support primitive declarations`() {
         parseAndAssert(
             input = """
                 class Test {
-                    boolean b;
-                    boolean b0 = true;
-                    boolean b1 = false;
-                }
-            """,
-            expected = buildAst {
-                `class`("Test") {
-                    field(boolean("b"))
-                    field(boolean("b0", true))
-                    field(boolean("b1", false))
-                }
-            }
-        )
-    }
+                    boolean bl;
+                    boolean bl0 = true;
+                    boolean bl1 = false;
 
-    @Test
-    fun `should support byte declarations`() {
-        parseAndAssert(
-            input = """
-                class Test {
                     byte b;
                     byte b0 = 1;
                     byte b1 = 0x02;
-                }
-            """,
-            expected = buildAst {
-                `class`("Test") {
-                    field(byte("b"))
-                    field(byte("b0", 1))
-                    field(byte("b1", 2))
-                }
-            }
-        )
-    }
 
-    @Test
-    fun `should support char declarations`() {
-        parseAndAssert(
-            input = """
-                class Test {
                     char c;
                     char c0 = 1;
                     char c1 = 'a';
-                }
-            """,
-            expected = buildAst {
-                `class`("Test") {
-                    field(char("c"))
-                    field(char("c0", 1.toChar()))
-                    field(char("c1", 'a'))
-                }
-            }
-        )
-    }
 
-    @Test
-    fun `should support int declarations`() {
-        parseAndAssert(
-            input = """
-                class Test {
                     int i;
                     int i0 = 1;
                     int i1 = 0x02;
-                }
-            """,
-            expected = buildAst {
-                `class`("Test") {
-                    field(int("i"))
-                    field(int("i0", 1))
-                    field(int("i1", 2))
-                }
-            }
-        )
-    }
 
-    @Test
-    fun `should support long declarations`() {
-        parseAndAssert(
-            input = """
-                class Test {
                     long l;
                     long l0 = 1L;
                     long l1 = 2l;
-                }
-            """,
-            expected = buildAst {
-                `class`("Test") {
-                    field(long("l"))
-                    field(long("l0", 1L))
-                    field(long("l1", 2L))
-                }
-            }
-        )
-    }
 
-    @Test
-    fun `should support float declarations`() {
-        parseAndAssert(
-            input = """
-                class Test {
                     float f;
                     float f0 = 1;
                     float f1 = 2f;
                     float f2 = 3F;
                     float f3 = 0.4F;
-                }
-            """,
-            expected = buildAst {
-                `class`("Test") {
-                    field(float("f"))
-                    field(float("f0", 1.0f))
-                    field(float("f1", 2.0f))
-                    field(float("f2", 3.0f))
-                    field(float("f3", 0.4f))
-                }
-            }
-        )
-    }
 
-    @Test
-    fun `should support double declarations`() {
-        parseAndAssert(
-            input = """
-                class Test {
                     double d;
                     double d0 = 1;
                     double d1 = 2d;
@@ -162,6 +64,32 @@ class FieldDeclarationTest {
             """,
             expected = buildAst {
                 `class`("Test") {
+                    field(boolean("bl"))
+                    field(boolean("bl0", true))
+                    field(boolean("bl1", false))
+
+                    field(byte("b"))
+                    field(byte("b0", 1))
+                    field(byte("b1", 2))
+
+                    field(char("c"))
+                    field(char("c0", 1.toChar()))
+                    field(char("c1", 'a'))
+
+                    field(int("i"))
+                    field(int("i0", 1))
+                    field(int("i1", 2))
+
+                    field(long("l"))
+                    field(long("l0", 1L))
+                    field(long("l1", 2L))
+
+                    field(float("f"))
+                    field(float("f0", 1.0f))
+                    field(float("f1", 2.0f))
+                    field(float("f2", 3.0f))
+                    field(float("f3", 0.4f))
+
                     field(double("d"))
                     field(double("d0", 1.0))
                     field(double("d1", 2.0))
